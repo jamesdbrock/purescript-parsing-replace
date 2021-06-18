f1769c19d94e46e40443488359e3195de4104343
James Brock <jamesbrock@gmail.com>
2021-06-18 16:32:48 +0900

    Pin nixpkgs 20.09

--- shell.nix
+++ shell.nix
@@ -1,9 +1,15 @@
-{ pkgs ? import <nixpkgs> { }
+# { pkgs ? import <nixpkgs> { }
+{ pkgs ? import (builtins.fetchGit {
+  # https://github.com/NixOS/nixpkgs/releases/tag/20.09
+  url = "https://github.com/nixos/nixpkgs/";
+  ref = "refs/tags/20.09";
+  rev = "cd63096d6d887d689543a0b97743d28995bc9bc3";
+  }) {}
 }:
 let
   easy-ps = import (builtins.fetchGit {
     url = "https://github.com/justinwoo/easy-purescript-nix.git";
-    rev = "1ec689df0adf8e8ada7fcfcb513876307ea34226";
+    rev = "47bdc016c7d56e987ca1aca690b1d6c9816a8584";
   }) { inherit pkgs; };
 in
 pkgs.mkShell {
